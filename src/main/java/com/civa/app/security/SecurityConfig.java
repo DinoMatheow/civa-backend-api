1389a048ebd8ecc87876af95c888bcf4857e55fd
DinoMatheow <matius152014@gmail.com>
2026-07-27 22:49:29 -0500

    feat: implmentacion de Autorizacion con roles

--- src/main/java/com/civa/app/security/SecurityConfig.java
+++ src/main/java/com/civa/app/security/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -22,6 +23,7 @@ import lombok.RequiredArgsConstructor;
 
 @Configuration
 @RequiredArgsConstructor
+@EnableMethodSecurity
 public class SecurityConfig {
     private final UserDetailsService userDetailsService;
     private final JwtAuthEntryPoint jwtAuthEntryPoint;
@@ -36,7 +38,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth-> 
                     auth
                     .requestMatchers("/api/v1/auth/**").permitAll()
-                    .requestMatchers(HttpMethod.GET, "/buss/**" ).permitAll()
+                    // .requestMatchers(HttpMethod.GET, "/buss/**" ).permitAll()
                     .anyRequest().authenticated()
                  )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
